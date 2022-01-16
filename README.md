14ac3d2fc5dc9aea5ccb6684866b6023af966bce
b <vpbalint@silentsignal.hu>
2022-01-16 09:51:11 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Ghidra Check Protector
+======================
+
+Which non-trivial functions don't reference the stack canary checker (or other, user-defined function)?
+
+Place your cursor to the entry point of the protector function before executing the script!
+
+Complexity threashold for reporting needs to be tweaked in the source.
+
---
+++ checkprotector.py
@@ -0,0 +1,31 @@
+# Check which functions don't reference a "protector" function (e.g.: stack canary check)
+#@author buherator
+#@category _NEW_
+#@keybinding
+#@menupath
+#@toolbar
+
+from ghidra.program.util import CyclomaticComplexity
+from ghidra.util.task import TaskMonitor
+
+CC_THREASHOLD = 10
+
+cyclomaticComplexity = CyclomaticComplexity()
+listing = currentProgram.getListing()
+
+refs = getReferencesTo(currentAddress)
+
+user_funcs = set()
+
+for r in refs:
+    user_funcs.add(getFunctionContaining(r.getFromAddress()))
+
+f = getFirstFunction()
+while f is not None:
+    if f not in user_funcs:
+        cc = cyclomaticComplexity.calculateCyclomaticComplexity(
+            f, TaskMonitor.DUMMY)
+        if cc > CC_THREASHOLD:
+            print("%s (cc: %d) does not reference %x" %
+                  (f, cc, currentAddress.getOffset()))
+    f = getFunctionAfter(f)
